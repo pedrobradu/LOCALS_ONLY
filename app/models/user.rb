934dad1459f9b2858345088aa385967036fadaeb
PEDRO DUARTE <pedrobradu@gmail.com>
2022-08-30 11:51:19 -0300

    relação polimórfica de tagable com user e activity feita

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :wishlists
   has_many :reviews
-  has_many :linked_tags
+
+  include Tagable
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
