a46d2982eaac96a050e6bb8516ff25bc9b046d21
Caroline Brum <carolinefbrum@gmail.com>
2022-08-31 10:44:15 -0300

    arrumei algumas identacoes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
-    has_many :wishlist_itens
-    has_many :reviews
+  has_many :wishlist_itens
+  has_many :reviews
 
-    include Tagable
+  include Tagable
 end
