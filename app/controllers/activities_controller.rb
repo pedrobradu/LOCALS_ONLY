ed058ee0937e1e3af2713028abbb788a27fe2e4a
Caroline Brum <carolinefbrum@gmail.com>
2022-08-31 11:24:21 -0300

    atualização da rota ok e adição de botão de voltar

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
   def index
     # @activities = Activity.all
+    # Se a gente quiser mostrar todas as activites, tem que fazer um if pra essa lógica funcionar. 
     @activities = Activity.where(category: params[:category])
   end
 
