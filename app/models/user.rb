a1b7576939a42c39ef588ac5e2f1233cae5e8313
PEDRO DUARTE <pedrobradu@gmail.com>
2022-09-02 14:49:13 -0300

    ajustes nos botões de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,18 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  after_create :user_wishlist
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def user_wishlist
+    wishlist = Wishlist.new
+    wishlist.user = self
+    wishlist.title = "Meus Favoritos:"
+    wishlist.save
+  end
 
 end
