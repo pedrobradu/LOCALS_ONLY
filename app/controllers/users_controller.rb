98f8b9797fe0eba75de890682a273c9c7effea45
Mauricio Pernidji <mauriciopernidji@gmail.com>
2022-09-02 14:31:21 -0300

    botao delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @wishlists = current_user.wishlists
+    @wishlist = Wishlist.find_by(user: current_user)
   end
+
 end
