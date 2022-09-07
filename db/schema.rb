ff79320de43aeb30d0b25d0a54a6ef4be92a7ac3
PEDRO DUARTE <pedrobradu@gmail.com>
2022-09-07 17:48:40 -0300

    _cards.scss

--- db/schema.rb
+++ db/schema.rb
@@ -136,7 +136,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_204220) do
     t.string "state"
     t.float "latitude"
     t.float "longitude"
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
