7ea9ab5f2bff8c0de914aecb0d585dac77e8b825
Nathalia Pinho <nathaliapinho.d@gmail.com>
2022-09-08 16:13:58 -0300

    chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,7 +5,7 @@ class ChatroomsController < ApplicationController
         profile_user_chats = profile_user.chatrooms
         @chatroom = (current_user_chats & profile_user_chats).first
         if @chatroom.nil?
-            @chatroom = Chatroom.create(name: "Chat com #{current_user.name} e #{profile_user.name}")
+            @chatroom = Chatroom.create(name: "#{current_user.name} e #{profile_user.name}")
             @chatroom.chat_memberships.create(user: current_user)
             @chatroom.chat_memberships.create(user: profile_user)
         end
